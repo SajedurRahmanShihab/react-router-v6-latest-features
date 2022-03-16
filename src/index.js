92455e8c2870c7be493475f3fcf27b559d5c39e7
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2022-03-17 01:35:07 +0600

    changes

--- src/index.js
+++ src/index.js
@@ -1,50 +1,171 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
 
 
-import { BrowserRouter as Router, Routes, Route, Navigate, Link, Outlet, useParams, NavLink } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route, Navigate, Link, Outlet, useParams, NavLink, useNavigate, useLocation } from 'react-router-dom'
+
+// ReactDOM.render(
+//   <Router>
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/myapps" element={<Navigate replace to="/learn" />} />
+//       <Route path="/learn" element={<Learn />} >
+//         <Route path="courses" element={<Courses />} >
+//           <Route path=":courseid" element={<CourseId />} />
+//         </Route>
+//         <Route path="bundles" element={<Bundles />} />
+//       </Route>
+//       <Route path="/dashboard" element={<Dashboard />} />
+//     </Routes>
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home route</h1>
+//     </div>
+//   )
+// }
+// function Learn() {
+//   return (
+//     <div>
+//       <h1>Learn</h1>
+//       <h4>All courses are listed here</h4>
+//       <Link className="btn btn-success" to="/learn/courses">Courses</Link> |
+//       <Link className="btn btn-primary" to="/learn/bundles">Bundles</Link>
+//       <Outlet />
+//     </div>
+//   )
+// }
+
+// function Courses() {
+//   const courseList = ["React", "Angular", "Vue", "Nodes"];
+//   const randomCourseName = courseList[Math.floor(Math.random() * courseList.length)];
+//   return (
+//     <div>
+//       <h1>Courses list</h1>
+//       <h4>Courses card</h4>
+
+//       <p>More test</p>
+//       <NavLink
+//         style={(isActive) => {
+//           return {
+//             backgroundColor: isActive ? "pink" : "blue"
+//           }
+//         }}
+//         to={`/learn/courses/${randomCourseName}`}>{randomCourseName}</NavLink>
+//       <NavLink className="btn btn-light" to={`/learn/courses/tests`}>tests</NavLink>
+//       <Outlet />
+//     </div>
+//   )
+// }
+// function Bundles() {
+//   return (
+//     <div>
+//       <h1>Bundle list</h1>
+//       <h4>Bundle card</h4>
+//     </div>
+//   )
+// }
+// function CourseId() {
+//   const navigate = useNavigate()
+//   const { courseid } = useParams();
+//   return (
+//     <div>
+//       <h1>URL Params is: {courseid}</h1>
+//       <button
+//         onClick={() => {
+//           navigate("/dashboard", { state: courseid });
+//         }}
+//         className="btn btn-warning">Price</button>
+//       <Link to="/dashboard" state={"Django"}>Test link</Link>
+//     </div> 
+//   )
+// }
+
+// function Dashboard() {
+//   const location = useLocation();
+//   return (
+//     <div>
+//       <h1>Info i go here is {location.state}</h1>
+//     </div>
+//   )
+// }
+
+
+
+
+// reportWebVitals();
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/myapps" element={<Navigate replace to="/learn" />} />
-      <Route path="/learn" element={<Learn />} >
-        <Route path="courses" element={<Courses />} >
+      <Route path="learn" element={<Learn />}>
+        <Route path="courses" element={<Courses />}>
           <Route path=":courseid" element={<CourseId />} />
         </Route>
         <Route path="bundles" element={<Bundles />} />
       </Route>
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 function Home() {
   return (
     <div>
       <h1>Home route</h1>
     </div>
-  )
+  );
 }
+
 function Learn() {
   return (
     <div>
       <h1>Learn</h1>
       <h4>All courses are listed here</h4>
-      <Link className="btn btn-success" to="/learn/courses">Courses</Link> |
-      <Link className="btn btn-primary" to="/learn/bundles">Bundles</Link>
+      <Link className="btn btn-success" to="/learn/courses">
+        courses
+      </Link>{" "}
+      |
+      <Link className="btn btn-primary" to="/learn/bundles">
+        bundle
+      </Link>
       <Outlet />
     </div>
-  )
+  );
 }
 
 function Courses() {
-  const courseList = ["React", "Angular", "Vue", "Nodes"];
-  const randomCourseName = courseList[Math.floor(Math.random() * courseList.length)];
+  const courseList = ["React", "Angular", "Vue", "Nodejs"];
+  const randomCourseName =
+    courseList[Math.floor(Math.random() * courseList.length)];
   return (
     <div>
       <h1>Courses list</h1>
@@ -52,16 +173,22 @@ function Courses() {
 
       <p>More test</p>
       <NavLink
-        style={(isActive) => {
+        style={({ isActive }) => {
           return {
-            backgroundColor: isActive ? "pink" : "blue"
-          }
+            backgroundColor: isActive ? "pink" : "yellow",
+          };
         }}
-        to={`/learn/courses/${randomCourseName}`}>{randomCourseName}</NavLink>
-      <NavLink className="btn btn-light" to={`/learn/courses/tests`}>tests</NavLink>
+        to={`/learn/courses/${randomCourseName}`}
+      >
+        {randomCourseName}
+      </NavLink>
+      <NavLink className="btn btn-light" to={`/learn/courses/tests`}>
+        tests
+      </NavLink>
+
       <Outlet />
     </div>
-  )
+  );
 }
 function Bundles() {
   return (
@@ -69,19 +196,37 @@ function Bundles() {
       <h1>Bundle list</h1>
       <h4>Bundle card</h4>
     </div>
-  )
+  );
 }
 function CourseId() {
+  const navigate = useNavigate();
   const { courseid } = useParams();
   return (
     <div>
-      <h1>URL Params is: {courseid}</h1>
-      <button className="btn btn-warning">Price</button>
+      <h1>URL Params is : {courseid}</h1>
+      <button
+        onClick={() => {
+          navigate("/dashboard", { state: courseid });
+        }}
+        className="btn btn-warning"
+      >
+        Price
+      </button>
+      <Link to="/dashboard" state={"DJANGO"}>
+        Test link
+      </Link>
     </div>
-  )
+  );
 }
 
-
-
+function Dashboard() {
+  const location = useLocation();
+  return (
+    <div>
+      <h1>Info that i got here is {location.state}</h1>
+    </div>
+  );
+}
 
 reportWebVitals();
+
